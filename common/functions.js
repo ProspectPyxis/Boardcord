6b0f0a8c7f23432c42449347c519e57a5ef7fd40
ProspectPyxis <prospectpyxis@gmail.com>
2020-11-28 14:06:25 +0700

    New utility functions
    - compareArrays
    - union2DArrays

--- common/functions.js
+++ common/functions.js
@@ -47,4 +47,42 @@ module.exports = (bot) => {
         return objects.every(object => union.size === Object.keys(object).length);
     }
 
+    // Code copied from stackexchange
+    // TODO: Refactor this to be able to handle unlimited arrays at once
+    bot.utils.compareArrays = (a1, a2) => {
+        // if the other array is a falsy value, return
+        if (!a1 || !a2)
+            return false;
+
+        // compare lengths - can save a lot of time
+        if (a1.length != a2.length)
+            return false;
+
+        for (var i = 0, l = a1.length; i < l; i++) {
+            // Check if we have nested arrays
+            if (a1[i] instanceof Array && a2[i] instanceof Array) {
+                // recurse into the nested arrays
+                if (!bot.utils.compareArrays(a1, a2))
+                    return false;
+            } else if (a1[i] != a2[i]) {
+                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bot.utils.union2DArrays = (...a) => {
+        let arr = [];
+
+        for (const i of a) {
+            for (const j of i) {
+                if (j.constructor !== Array) continue;
+                if (!arr.some(e => bot.utils.compareArrays(e, j))) arr.push(j);
+            }
+        }
+
+        return arr;
+    }
+
 }
