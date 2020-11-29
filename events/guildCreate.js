9adab89cabc34727d96d030570acf34a26fde9e1
ProspectPyxis <prospectpyxis@gmail.com>
2020-11-30 00:15:42 +0700

    Missed one

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,5 @@
 module.exports = async (bot, guild) => {
-    let str = "**Thanks for adding Boardcord!** :wave:\n\n";
+    let str = "**Thanks for adding GeMatrix!** :wave:\n\n";
     str += "This bot allows you to play quick games with your friends right in Discord itself - no other software needed!\n";
     str += "To get started, please see the \"Getting Started\" page of the documentation here: <https://prospectpyxis.github.io/GeMatrix/pages/guide.html>\n\n";
     str += "I hope you enjoy the games we have!";
