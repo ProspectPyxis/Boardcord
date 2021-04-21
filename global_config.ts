0108165c835bcef2702eff5c42792b801cb32f09
ProspectPyxis <prospectpyxis@gmail.com>
2021-04-21 10:06:37 +0700

    refactor: beginning of typescript rewrite

---
+++ global_config.ts
@@ -0,0 +1,18 @@
+// NOTE: Remember to sync this file to global_config.ts.example when changed!
+
+const config = {
+    // Replace this with your own bot token!
+    "token": "Nzc0OTE2Njg0NjIwMzY1ODU1.X6evZg.AVsxRhLIs8ys18zXgoI_z_CCWvQ",
+
+    // Put your own user ID here to identify yourself to the bot as its owner
+    "ownerID": "252256413764550656",
+
+    // This is the prefix the bot will use by default, if no server-specific prefix is set
+    "prefix": "gm!",
+
+    // This is the limit on how many games can be run in each server at once
+    // Keep this low in order to save on memory usage
+    "gamesPerServer": 1
+}
+
+module.exports = config
